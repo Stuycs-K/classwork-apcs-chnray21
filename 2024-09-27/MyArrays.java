a199773989b70a8e669911c388bc67397e1366c0
Raymond Chen <rchen60@stuy.edu>
2024-09-27 11:43:34 -0400

    Created two test cases for returnCopy, added in arraytoString, 27 Sept 2024

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,32 @@
+public class MyArrays {
+  public static String arrayToString(int[] nums) {
+    string ans = "[";
+    for (int i-0; i<nums.length; i++) {
+      ans += nums[i];
+      ans = ans + ", ";
+    }
+    ans = ans.substring(0, ans.length()-2) + "]";
+    return ans;
+  }
+
+  public static int[] returnCopy(int[] ary) {
+    
+  }
+
+  public static int[] concatArray(int[] ary1, int[] ary2) {
+
+  }
+
+  public static void main(String[] args) {
+    int[] testArray = new int {1, 2, 3, 4};
+    int[] newArray = returnCopy(testArray);
+    System.out.println(arrayToString(testArray) == arrayToString(newArray)
+                        && testArray != newArray);
+
+    int[] testArray = new int {};
+    int[] newArray = returnCopy(testArray);
+    System.out.println(arrayToString(testArray) == arrayToString(newArray)
+                        && testArray != newArray);
+
+  }
+}
