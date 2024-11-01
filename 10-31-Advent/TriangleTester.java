5b3ea5b0d92a1ea0d11ee3ca4ada571a5f50431c
Raymond Chen <chn.ray21@gmail.com>
2024-11-01 19:54:25 -0400

    countTrianglesB successful

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -40,7 +40,9 @@ public class TriangleTester {
         int b = input.nextInt();
         input.nextLine();
         int c = input.nextInt();
-        input.nextLine();
+        if (input.hasNextLine()) {
+          input.nextLine();
+        }
         if (a+b>c && a+c>b && c+b>a) {
           num++;
         }
@@ -56,7 +58,31 @@ public class TriangleTester {
         input2.nextLine();
         input2.nextInt();
         int c = input2.nextInt();
-        input2.nextLine();
+        if (input2.hasNextLine()) {
+          input2.nextLine();
+        }
+
+        if (a+b>c && a+c>b && c+b>a) {
+          num++;
+        }
+      }
+
+      Scanner input3 = new Scanner(myFile);
+      while (input3.hasNextLine()) {
+        input3.nextInt();
+        input3.nextInt();
+        int a = input3.nextInt();
+        input3.nextLine();
+        input3.nextInt();
+        input3.nextInt();
+        int b = input3.nextInt();
+        input3.nextLine();
+        input3.nextInt();
+        input3.nextInt();
+        int c = input3.nextInt();
+        if (input3.hasNextLine()) {
+          input3.nextLine();
+        }
         if (a+b>c && a+c>b && c+b>a) {
           num++;
         }
@@ -64,6 +90,7 @@ public class TriangleTester {
 
       input.close();
       input2.close();
+      input3.close();
       return num;
     }
     catch (FileNotFoundException ex) {
@@ -73,7 +100,7 @@ public class TriangleTester {
   }
 
   public static void main(String[] args) {
-    // System.out.println(countTrianglesA("inputTri.txt"));
+    System.out.println(countTrianglesA("inputTri.txt"));
     System.out.println(countTrianglesB("inputTri.txt"));
   }
 }
