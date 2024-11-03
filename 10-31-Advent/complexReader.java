443323aa2efa5333bc1a4bf4c5e2d7d2bc0b3da5
Raymond Chen <chn.ray21@gmail.com>
2024-11-03 10:43:54 -0500

    Created code to read X-separated values in a text file

---
+++ 10-31-Advent/complexReader.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class complexReader {
+  public static ArrayList<Integer> reader(String filename) {
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+
+      
+    }
+  }
+}
