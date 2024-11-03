0661ea47eeeae502b496795183b1e71088f5c6f1
Raymond Chen <chn.ray21@gmail.com>
2024-11-03 10:53:17 -0500

    complexReader reads a file and prints out values in the file as an arraylist successfully

--- 10-31-Advent/complexReader.java
+++ 10-31-Advent/complexReader.java
@@ -5,11 +5,32 @@ import java.util.ArrayList;
 
 public class complexReader {
   public static ArrayList<Integer> reader(String filename) {
+    ArrayList<Integer>result = new ArrayList<Integer>();
     try {
       File file = new File(filename);
       Scanner input = new Scanner(file);
-
       
+      while (input.hasNextLine()) {
+        String line = input.nextLine();
+        String[] nums = line.split("x");
+        
+        for (int i=0; i<nums.length; i++) {
+          result.add(Integer.parseInt(nums[i]));
+        }
+      }
+
+      input.close();
+      return result;
     }
+    catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return result;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(reader("sepX.txt"));
+
+    
   }
 }
