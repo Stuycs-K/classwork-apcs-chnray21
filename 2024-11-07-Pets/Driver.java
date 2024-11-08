b056c345c75282d05638993ff6fdc7716fcca87f
Raymond Chen <chn.ray21@gmail.com>
2024-11-07 23:19:40 -0500

    Tested assigning which types can be assigned to which variables

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -3,9 +3,22 @@
  */
 public class Driver {
   public static void main(String[] args) {
-    Animal Bob = new Animal("meow", 2909, "Bob");
-    Bob.speak();
-    Bird BobII = new Bird("woof", 2908, "BobII", 1, "green");
-    BobII.speak();
+    // Animal Bob = new Animal("meow", 2909, "Bob");
+    // Bob.speak();
+    // Bird BobII = new Bird("woof", 2908, "BobII", 1, "green");
+    // BobII.speak();
+
+    Animal a1 = new Animal("kaboom", 2, "Explosive");
+    a1.speak();
+
+    Bird b1 = new Bird("cheep", 3, "TheBird", 5, "blue");
+    b1.speak();
+
+    // Bird b2 = new Animal("caw", 1, "Crow");
+    // b2.speak();
+    // Doesn't work because Animal is not a child of Bird
+
+    Animal a2 = new Bird ("caw", 1, "Crow", 6, "black");
+    a2.speak();
   }
  }
