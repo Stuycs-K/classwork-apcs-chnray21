860e2ed0b3ab4c74d3c314ca40cf03a7f9072aad
Raymond Chen <rchen60@stuy.edu>
2024-11-14 11:28:55 -0500

    createRandomArray() completed; tested to see if makes random numbers

---
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayListPractice {
+  public static ArrayList<String>createRandomArray(int size) {
+    ArrayList<String>result = new ArrayList<String>(size);
+    for (int i = 0; i<size; i++) {
+      int num = (int)(10*Math.random());
+      System.out.println(num);
+      if (num == 0) {
+        result.add(i, "");
+      }
+      else {
+        result.add(i, ""+num);
+      }
+    }
+    return result;
+  }
+}
