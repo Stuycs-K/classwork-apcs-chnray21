59b74d549fccb345734a5dab4bdaea1f59f9d54b
Raymond Chen <rchen60@stuy.edu>
2024-11-19 11:40:31 -0500

    day2 can read the file and print each line

--- 2024-11-18-advent2016/day2.java
+++ 2024-11-18-advent2016/day2.java
@@ -3,18 +3,31 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 
 public class day2 {
-  public static int fxn(String filename) {
+  public static String fxn(String filename) {
     try {
       File myFile = new File(filename);
       Scanner input = new Scanner(myFile);
+      // if up, subtract 3
+      // if right, add 1
+      // if down, add 3
+      // if left, subtract 1
 
-      return 1;
+      String result = "yippee";
+      int value = 5;
 
+      while (input.hasNextLine()) {
+        String command = input.nextLine();
+        System.out.println(command);
+        result+=value;
+      }
+
+      input.close();
+      return result;
     }
 
     catch (FileNotFoundException ex) {
       System.out.println("File not found");
-      return -1;
+      return "invalid";
     }
 
   }
