30b89d235b7df2dac3e44b55f34228eed5785a5a
Raymond Chen <chn.ray21@gmail.com>
2024-11-18 22:00:43 -0500

    Created method, returns a valid value

--- 2024-11-18-advent2016/day1.java
+++ 2024-11-18-advent2016/day1.java
@@ -12,18 +12,37 @@ public class day1 {
 
       int nsew = 1;
       // 1 = north, 2 = east, 3 = south, 4 = west
+      int result = 0;
 
       for (int i =0; i<directions.length; i++) {
-        // System.out.println(direction[i]);
+        // get the direction and how much to travel forwards
         String nsewTemp = directions[i].substring(0, 1);
         int forward = Integer.parseInt(directions[i].substring(1));
-        // get the direction and how much to travel forwards
-        System.out.println(nsewTemp + ", " + forward);
 
-        // add or subtract the value
+        // +1 or -1 to nsew depending on if direction is R or L
+        if (nsewTemp == "R") {
+          nsew++;
+        }
+        else {
+          nsew--;
+        }
+
+        // change nsew to fit the the range of values from 1 to 4 
+        // if it is not there
+        if (nsew == 0) {
+          nsew = 4;
+        }
+        else if (nsew == 5) {
+          nsew = 1;
+        }
+
+        if (nsew == 1 || nsew == 2) {
+          result+=forward;
+        }
+        else if (nsew == 3 || nsew == 4) {
+          result-=forward;
+        }
       }
-
-      int result = 0;
 
       input.close();
       return result;
