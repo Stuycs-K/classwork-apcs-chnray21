4d0ba8cd9228cced4d8d33563b4b4d1d72dd1cbb
Raymond Chen <chn.ray21@gmail.com>
2024-11-18 21:37:28 -0500

    Wrote basic code for scanner, successfully scans the file and prints values

--- 2024-11-18-advent2016/day1.java
+++ 2024-11-18-advent2016/day1.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class day1 {
-    
+  public static int distance(String filename) {
+    try {
+      File myFile = new File(filename);
+      Scanner input = new Scanner(myFile);
+      String[] direction = input.nextLine().split(", ");
+      for (int i =0; i<direction.length; i++) {
+        System.out.println(direction[i]);
+      }
+
+      int result = 0;
+
+      input.close();
+      return result;
+    }
+    catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return -1;
+    }
+  }  
+
+  public static void main(String[] args) {
+    System.out.println(distance("inputDay1.txt"));
+  }
 }
