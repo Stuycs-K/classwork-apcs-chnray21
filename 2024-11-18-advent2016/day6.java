04df3df0b73f6b2e12e376d8e60ffbc4a8bed3cf
Raymond Chen <chn.ray21@gmail.com>
2024-11-23 19:25:05 -0500

    completed day6

--- 2024-11-18-advent2016/day6.java
+++ 2024-11-18-advent2016/day6.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class day6 {
   public static String fxn(String filename) {
     try {
-      File myFile = new File(filename);
-      Scanner input = new Scanner(myFile);
-      
       String result = "";
 
-      for (int i=0; i<1; i++) {
+      for (int i=0; i<8; i++) {
+        File myFile = new File(filename);
+        Scanner input = new Scanner(myFile);
+      
         int[] alpha = new int[26];
         // add in values of each letter in the alphabet for the column
         while (input.hasNextLine()) {
@@ -19,6 +19,9 @@ public class day6 {
           alpha[letter - 'a'] = alpha[letter - 'a'] + 1;
         } 
         
+        input.close();
+
+
         int index = 0; // records the index of the highest frequency letter
         int value = 0; // records the value of the highest frequency
         // for loop to find the index of the most frequent letter
@@ -29,13 +32,12 @@ public class day6 {
           }
         }
 
-        System.out.println(index);
-        System.out.println(value);
         // add the most frequent letter to result
+        char letter = (char)('a'+index);
+        result+=letter;
 
       }
 
-      input.close();
       System.out.println(" ");
       return result;
     }
@@ -46,7 +48,51 @@ public class day6 {
     }
   }
 
+  public static String fxn2(String filename) {
+    try {
+      String result = "";
+
+      for (int i=0; i<8; i++) {
+        File myFile = new File(filename);
+        Scanner input = new Scanner(myFile);
+      
+        int[] alpha = new int[26];
+        // add in values of each letter in the alphabet for the column
+        while (input.hasNextLine()) {
+          String line = input.nextLine();
+          char letter = line.charAt(i);
+          alpha[letter - 'a'] = alpha[letter - 'a'] + 1;
+        } 
+        
+        input.close();
+
+        int index = 0; // records the index of the lowest frequency letter
+        int value = 1000; // records the value of the lowest frequency
+        // for loop to find the index of the least frequent letter
+        for (int j=0; j<alpha.length; j++) {
+          if (alpha[j] < value) {
+            index = j;
+            value = alpha[j];
+          }
+        }
+
+        // add the most frequent letter to result
+        char letter = (char)('a'+index);
+        result+=letter;
+
+      }
+
+      System.out.println(" ");
+      return result;
+    }
+
+    catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return "invalid";
+    }
+  }
   public static void main(String[] args) {
-    System.out.println(fxn("inputDay6.txt"));
+    // System.out.println(fxn("inputDay6.txt"));
+    System.out.println(fxn2("inputDay6.txt"));
   }
 }
