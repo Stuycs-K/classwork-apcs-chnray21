9c6fe284b8ce6bc4d35672cc3f0e73fa4f555ba9
Raymond Chen <chn.ray21@gmail.com>
2024-11-23 21:30:48 -0500

    day4 part2 complete

--- 2024-11-18-advent2016/day4.java
+++ 2024-11-18-advent2016/day4.java
@@ -64,7 +64,43 @@ public class day4 {
     }
   }
 
+  public static void fxn2(String filename) {
+    try {
+      File myFile = new File(filename);
+      Scanner input = new Scanner(myFile);
+
+      while (input.hasNextLine()) {
+        String line = input.nextLine();
+        String[] splitLine = line.split("-");
+        // splits lines by dashes so that the last element of the array is the sector ID and checksum
+
+        int shiftNum = Integer.parseInt(splitLine[splitLine.length-1].substring(0, 3));
+        String result = "";
+
+        for (int i=0; i<splitLine.length-1; i++) {
+          for (int j=0; j<splitLine[i].length(); j++) {
+            if (splitLine[i].charAt(j)+ shiftNum%26 > 'z') {
+              result += (char)('a'+(splitLine[i].charAt(j)+(shiftNum%26) - 'z'));
+            }
+            else {
+              result += (char)(splitLine[i].charAt(j)+(shiftNum%26));
+            }
+          }
+          result+="-";
+        }
+
+        System.out.println(result+shiftNum);
+      }
+
+      input.close();
+    }
+    catch (FileNotFoundException ex){
+      System.out.println("File not found");
+    }
+  }
+
   public static void main(String[] args) {
-    System.out.println(fxn("inputDay4.txt"));
+    //System.out.println(fxn("inputDay4.txt"));
+    fxn2("inputDay4.txt");
   }
 }
