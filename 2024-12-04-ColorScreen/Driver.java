599a5a3a17cd003f3662c7772697486fbe9ec4a2
Raymond Chen <rchen60@stuy.edu>
2024-12-04 11:21:17 -0500

    Did some initial stuff

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,5 +1,12 @@
 public class Driver {
+  public static void makeBorder(color) {
+
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println(CLEAR_SCREEN);
+    System.out.println(HIDE_CURSOR);
+
+
   }
 }
