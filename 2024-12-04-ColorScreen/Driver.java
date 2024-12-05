f55eae4f57162f836b463fa90ca8561dcb55029b
Raymond Chen <chn.ray21@gmail.com>
2024-12-04 21:58:35 -0500

    Created right side of border

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -3,16 +3,22 @@ public class Driver {
     System.out.println(Text.CLEAR_SCREEN);
     System.out.println(Text.HIDE_CURSOR);
 
-    for (int i=0; i<80; i++) {
+    for (int i=1; i<81; i++) {
       Text.color(Text.background(color));
       Text.go(0, i);
       System.out.println("~");
     }
 
+    for (int i=2; i<31; i++) {
+      Text.color(Text.background(color));
+      Text.go(i, 80);
+      System.out.println("~");
+    }
     System.out.println(Text.RESET);
   }
 
   public static void main(String[] args) {
     makeBorder(Text.BLUE);
+
   }
 }
