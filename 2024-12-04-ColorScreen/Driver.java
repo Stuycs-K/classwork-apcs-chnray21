6b64e5aeaccd599c6797142bc51dcc832e26c3be
Raymond Chen <chn.ray21@gmail.com>
2024-12-04 22:36:18 -0500

    Added the numbers part

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,7 +1,8 @@
 public class Driver {
   public static void makeBorder(int color) {
-    System.out.println(Text.CLEAR_SCREEN);
     System.out.println(Text.HIDE_CURSOR);
+
+    // make the frame
 
     for (int i=1; i<81; i++) {
       Text.color(Text.background(color));
@@ -27,13 +28,34 @@ public class Driver {
       System.out.println("~");
     }
     
-    System.out.println(Text.RESET);
+    // make the numbers
+    int[] nums = createRandomArray(3);
+
+    for (int i=1; i<4; i++) {
+      if (nums[i-1] < 25 ) {
+        Text.go(1, i+(20*i));
+        Text.color(Text.RED, Text.BRIGHT);
+        System.out.print(nums[i-1]);
+      }
+      else if (nums[i-1] > 75) {
+        Text.go(1, i+(20*i));
+        Text.color(Text.GREEN, Text.BRIGHT);
+        System.out.print(nums[i-1]);
+      }
+      else {
+        Text.go(1, i+(20*i));
+        Text.color(Text.WHITE);
+        System.out.print(nums[i-1]);
+      }
+    }
+
+    System.out.println(Text.SHOW_CURSOR);
   }
 
   public static int[] createRandomArray(int size) {
     int[] result = new int[size];
     for (int i = 0; i<size; i++) {
-      int num = (int)(1000*Math.random());
+      int num = (int)(100*Math.random());
       result[i] = num;
     }
     return result;
@@ -41,6 +63,5 @@ public class Driver {
 
   public static void main(String[] args) {
     makeBorder(Text.RED);
-
   }
 }
