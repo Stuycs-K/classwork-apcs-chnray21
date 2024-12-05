47ef3df3ffaedf98c86d82f5b1bbe0c733db0534
Raymond Chen <chn.ray21@gmail.com>
2024-12-04 22:43:49 -0500

    Nothing is working, will restart laptop b/c seems to be crashing

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,55 +1,56 @@
 public class Driver {
   public static void makeBorder(int color) {
-    System.out.println(Text.HIDE_CURSOR);
+    System.out.print(Text.HIDE_CURSOR);
 
     // make the frame
 
     for (int i=1; i<81; i++) {
-      Text.color(Text.background(color));
+      Text.background(color);
       Text.go(0, i);
-      System.out.println("~");
-    }
-
-    for (int i=2; i<31; i++) {
-      Text.color(Text.background(color));
-      Text.go(i, 80);
-      System.out.println("~");
-    }
-
-    for (int i=1; i<80; i++) {
-      Text.color(Text.background(color));
-      Text.go(29, i);
       System.out.print("~");
     }
 
-    for (int i=1; i<29; i++) {
-      Text.color(Text.background(color));
-      Text.go(i, 0);
-      System.out.println("~");
-    }
+    // for (int i=2; i<31; i++) {
+    //   Text.background(color);
+    //   Text.go(i, 80);
+    //   System.out.println("~");
+    // }
+
+    // for (int i=1; i<80; i++) {
+    //   Text.background(color);
+    //   Text.go(29, i);
+    //   System.out.print("~");
+    // }
+
+    // for (int i=1; i<29; i++) {
+    //   Text.background(color);
+    //   Text.go(i, 0);
+    //   System.out.print("~");
+    // }
     
     // make the numbers
-    int[] nums = createRandomArray(3);
+    // int[] nums = createRandomArray(3);
 
-    for (int i=1; i<4; i++) {
-      if (nums[i-1] < 25 ) {
-        Text.go(1, i+(20*i));
-        Text.color(Text.RED, Text.BRIGHT);
-        System.out.print(nums[i-1]);
-      }
-      else if (nums[i-1] > 75) {
-        Text.go(1, i+(20*i));
-        Text.color(Text.GREEN, Text.BRIGHT);
-        System.out.print(nums[i-1]);
-      }
-      else {
-        Text.go(1, i+(20*i));
-        Text.color(Text.WHITE);
-        System.out.print(nums[i-1]);
-      }
-    }
+    // for (int i=1; i<4; i++) {
+    //   if (nums[i-1] < 25 ) {
+    //     Text.go(1, i+(20*i));
+    //     Text.color(Text.RED, Text.BRIGHT);
+    //     System.out.print(nums[i-1]);
+    //   }
+    //   else if (nums[i-1] > 75) {
+    //     Text.go(1, i+(20*i));
+    //     Text.color(Text.GREEN, Text.BRIGHT);
+    //     System.out.print(nums[i-1]);
+    //   }
+    //   else {
+    //     Text.go(1, i+(20*i));
+    //     Text.color(Text.WHITE);
+    //     System.out.print(nums[i-1]);
+    //   }
+    // }
 
-    System.out.println(Text.SHOW_CURSOR);
+   //  Text.go(31, 0);
+    System.out.print(Text.SHOW_CURSOR);
   }
 
   public static int[] createRandomArray(int size) {
