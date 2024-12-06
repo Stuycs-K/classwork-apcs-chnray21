27ade081fa352a7d9fe155164e9991b6721ab5a4
Raymond Chen <rchen60@stuy.edu>
2024-12-06 11:44:23 -0500

    Finished attack/support functions

--- 2024-12-06-Adventurer/Assassin.java
+++ 2024-12-06-Adventurer/Assassin.java
@@ -16,4 +16,25 @@ public class Assassin extends Adventurer {
   public int getSpecialMax {
     return maxAccuracy;
   }
+
+  public String attack(Adventurer other) {
+    other.setSpecial(other.getSpecial - 1);
+    return "Assassin stabbed "+other;
+  }
+
+  public String support(Adventurer other) {
+    other.setHP(other.getHP()-1);
+    return "Assassin gave a healing potion to "+other;
+  }
+
+  public String support() {
+    this.setHP(this.getHP()-1);
+    return "Assassin drank a healing potion";
+  }
+
+  public String specialAttack(Adventurer other) {
+    other.setHP(other.getHP()-this.getSpecial());
+    this.setSpecial(this.getSpecial()-2);
+    return "Assassin ambushed "+other;
+  }
 }
