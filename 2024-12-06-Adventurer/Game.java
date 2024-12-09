25595a70e7b6aec3cce36899824c754aad4fd1ef
Raymond Chen <rchen60@stuy.edu>
2024-12-09 11:43:38 -0500

    Created moveset

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
+
 public class Game {
   public static void main(String[] args) {
     Adventurer player = new CodeWarrior();
     Adventurer enemy = new CodeWarrior();
 
     boolean quit = false;
+    Scanner userInput = new Scanner(System.in);
 
     while (!quit) {
       System.out.println("Player:  HP: " + player.getHP() + ", Special Resource: " + player.getSpecial());
       System.out.println("Enemy: HP: " + enemy.getHP() + ", Special Resource: " + player.getSpecial());
 
-      
+      System.out.println("Choose move: (a)ttack / (sp)ecial / (su)pport / quit");
+      String move = userInput.nextLine();
+      if (move == "a" || move == "attack") {
+        System.out.println(player.attack(enemy));
+      }
+      else if (move == "sp" || move == "attack") {
+        System.out.println(player.specialAttack(enemy));
+      }
+      else if (move == "su" || move == "support") {
+        System.out.println(player.support());
+      }
+      else if (move == "quit") {
+        quit = true;
+      }
     }
   }
 }
