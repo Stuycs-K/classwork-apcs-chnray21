057f8185c2ee9e7b0540f7b12bdcbaf8f1c217a2
Raymond Chen <rchen60@stuy.edu>
2025-01-02 11:39:53 -0500

    Wrote and tested fibonacci method

--- 2025-01-02/fib.java
+++ 2025-01-02/fib.java
@@ -1,5 +1,17 @@
 public class fib {
+  public static int fib(int n) {
+    int sum = 0;
+    if (n<2) {
+      return n;
+    }
+    else {
+      sum += fib(n-1) + fib(n-2);
+    }
+    return sum;
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    System.out.println(8 == fib(6));
+    System.out.println(13 == fib(7));
   }
 }
