3895586d2ed0487db2490b5add23837ca8ac9962
Raymond Chen <chn.ray21@gmail.com>
2025-01-03 11:36:14 -0500

    Created file for makeWords

---
+++ 2025-01-02/makeWords.java
@@ -0,0 +1,9 @@
+public class makeWords {
+  public static void makeWords(int remainingLetters, String result, String alphabet) {
+    System.out.println("words");
+  }
+
+  public static void main(String[] args) {
+    makeWords(0, "hi", "yeah");
+  }
+}
