6e5be05eebf27cf5735515ad8e158e04b41d2762
Raymond Chen <chn.ray21@gmail.com>
2025-01-03 11:47:27 -0500

    Wrote makewords

--- 2025-01-02/makeWords.java
+++ 2025-01-02/makeWords.java
@@ -1,9 +1,18 @@
 public class makeWords {
   public static void makeWords(int remainingLetters, String result, String alphabet) {
-    System.out.println("words");
+    if (remainingLetters == 0) {
+      System.out.println(result);
+    }
+
+    else {
+      remainingLetters--;
+      for (int i=1; i<alphabet.length(); i++) {
+        result+=alphabet.substring(i-1, i);
+      }
+    }
   }
 
   public static void main(String[] args) {
-    makeWords(0, "hi", "yeah");
+    makeWords(2, "", "abc");
   }
 }
